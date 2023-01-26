dc05d960618ffa6ede89234ba6926f8d06225723
HUMBERT-DROZ-LAURENT Romain <Romain.HUMBERT-DROZ-LAURENT@cpnv.ch>
2023-01-26 15:23:27 +0100

    feat: CsvHelper

--- FileManager/CsvHelper.cs
+++ FileManager/CsvHelper.cs
@@ -3,20 +3,58 @@
     public class CsvHelper : FileHelper
     {
         #region private attributs
-        //TODO Private attributs - 2pts
+        private string _fileName;
+        private string _path;
         #endregion private attributs
 
         #region constructor
         public CsvHelper(string path, string fileName, char separator = ';') : base(path, fileName)
         {
-            //TODO Constructor - 3pts
+            _path = path;
+            _fileName = fileName;
+
+            if (!IsCharSupported(separator))
+            {
+                throw new UnsupportedSeparatorException();
+            }
+            else
+            {
+                _fullPath = path + @"//" + fileName;
+            }
         }
         #endregion constructor
 
         #region public methods 
-        public void ExtractFileContent()
+        public new void ExtractFileContent()
         {
             //TODO ExtractFileContent - 6pts
+
+            StreamReader streamReader = new StreamReader(_fullPath);
+            string line;
+
+            // Reads and stores lines from the file until eof.
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                this.Lines.Add(line);
+            }
+            streamReader.Close();
+
+            if (this.Lines.Count == 0)
+            {
+                throw new EmptyFileException();
+            }
+
+            foreach (string registeredLine in Lines)
+            {
+
+                string lineToTest = registeredLine.ToString();
+                //Si la partie en commentaire est retirée, le test 'NominalCase_GetFileContent ne passe plus'
+                //Cette partie devrait faire passer le dernier test, mais à la place, elle bloque l'autre.
+                if (lineToTest == "" /*|| lineToTest.Contains(";;")*/)
+                {
+                    throw new StructureException();
+                }
+            }
         }
         #endregion public methods
 
@@ -24,7 +62,14 @@
         private bool IsCharSupported(char separator)
         {
             //TODO IsCharSupported - 2pts
-            throw new NotImplementedException();
+            if (separator != ';')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
         #endregion privates methods
 
